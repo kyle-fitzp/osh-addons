3007ba83871dd288bfa95dcc80dad7ab79cabc3c
rmw-workbotts <157396854+rmw-workbotts@users.noreply.github.com>
2024-05-22 19:59:44 -0500

    Add files via upload (#3)
    Fixed crash on stop for driver.

---
+++ sensors/cyber/sensorhub-driver-sysinfo/sensorhub-driver-sysinfo/src/main/java/com/sample/impl/sensor/sysinfo/Alerts.java
@@ -0,0 +1,388 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ The contents of this file are subject to the Mozilla Public License, v. 2.0.
+ If a copy of the MPL was not distributed with this file, You can obtain one
+ at http://mozilla.org/MPL/2.0/.
+
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the License.
+
+ Copyright (C) 2024 Botts Innovative Research, Inc. All Rights Reserved.
+
+ ******************************* END LICENSE BLOCK ***************************/
+package com.sample.impl.sensor.sysinfo;
+
+import net.opengis.swe.v20.DataBlock;
+import net.opengis.swe.v20.DataComponent;
+import net.opengis.swe.v20.DataEncoding;
+import net.opengis.swe.v20.DataRecord;
+import org.sensorhub.api.data.DataEvent;
+import org.sensorhub.impl.sensor.AbstractSensorOutput;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.vast.swe.*;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.OSProcess;
+import oshi.software.os.OperatingSystem;
+
+import javax.mail.*;
+
+
+import java.util.Properties;
+/**
+ * Configuration settings for the {@link SystemsInfoSensor} driver exposed via the OpenSensorHub Admin panel.
+ * <p>
+ * Configuration settings take the form of
+ * <code>
+ * DisplayInfo(desc="Description of configuration field to show in UI")
+ * public Type configOption;
+ * </code>
+ * <p>
+ * Containing an annotation describing the setting and if applicable its range of values
+ * as well as a public access variable of the given Type
+ *
+ * @author Robin_White
+ * @since March 1st 2024
+ */
+
+
+public class Alerts extends AbstractSensorOutput<SystemsInfoSensor> implements Runnable {
+
+
+    OSProcess cpuVal;
+    SystemInfo si = new SystemInfo();
+    OperatingSystem os = si.getOperatingSystem();
+    CentralProcessor processor = si.getHardware().getProcessor();
+
+    HardwareAbstractionLayer hal = si.getHardware();
+    Properties prop = new Properties();
+
+
+    private static final String SENSOR_OUTPUT_NAME = "Systems info alerts";
+    private static final String SENSOR_OUTPUT_LABEL = "Systems info alerts";
+    private static final String SENSOR_OUTPUT_DESCRIPTION = "Alerts derived from computer system info metrics";
+    private static final Logger logger = LoggerFactory.getLogger(SystemsInfoOutput.class);
+    private static final int MAX_NUM_TIMING_SAMPLES = 10;
+    private final long[] timingHistogram = new long[MAX_NUM_TIMING_SAMPLES];
+    private DataRecord dataStruct;
+    private final Object histogramLock = new Object();
+    private DataEncoding dataEncoding;
+    private Boolean stopProcessing = false;
+    private Thread worker;
+    private final Object processingLock = new Object();
+    private int setCount = 0;
+    Properties properties = System.getProperties();
+    String alertTarget = SystemsInfoConfig.alertTarget;
+    String alertSender = SystemsInfoConfig.alertSender;
+    //TODO Set these values to passkey for google mail as described in readme document
+    Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(alertSender, "google mail pass key here");
+        }
+    });
+    Boolean CPUemailSent = false;
+    Boolean userEmailSent = false;
+    Boolean RAMemailSent = false;
+
+
+    Alerts(SystemsInfoSensor parentSystemsInfoSensor) {
+
+        super(SENSOR_OUTPUT_NAME, parentSystemsInfoSensor);
+
+        logger.debug("Output created");
+
+
+    }
+
+    public void doInit() {
+        logger.debug("Initializing Output");
+
+        // Get an instance of SWE Factory suitable to build components
+        SWEHelper sweFactory = new SWEHelper();
+
+        dataStruct = sweFactory.createRecord()
+                .name(SENSOR_OUTPUT_NAME)
+                .label(SENSOR_OUTPUT_LABEL)
+                .description(SENSOR_OUTPUT_DESCRIPTION)
+                .addField("SampleTime", sweFactory.createTime().asSamplingTimeIsoUTC().build())
+                .addField("cpuUsage", sweFactory.createQuantity()
+                        .definition(SWEHelper.getCfUri("CPU_Percent"))
+                        .label("Percent Usage of CPU")
+                        .uomCode("")
+                        .description("Percent Usage of the the CPU, Non-windows boxes may return above ")
+                )
+                .addField("ramUsage", sweFactory.createText()
+
+                        .label("Ram Usage")
+
+                        .description("OSHI HardwareLayer RAM use physical/available")
+
+                )
+
+
+                .build();
+        dataEncoding = sweFactory.newTextEncoding(",", "\n");
+        logger.debug("Initializing Output Complete");
+
+    }
+
+
+    public void doStart() {
+
+        // Instantiate a new worker thread
+        worker = new Thread(this, this.name);
+
+        // TODO: Perform other startup
+
+        logger.info("Starting worker thread: {}", worker.getName());
+
+        // Start the worker thread
+        worker.start();
+
+
+    }
+
+    public void doStop() {
+
+        synchronized (processingLock) {
+
+            stopProcessing = true;
+        }
+
+
+    }
+
+    public boolean isAlive() {
+        return worker.isAlive();
+
+
+    }
+
+    @Override
+    public DataComponent getRecordDescription() {
+
+        return dataStruct;
+    }
+
+    @Override
+    public DataEncoding getRecommendedEncoding() {
+
+        return dataEncoding;
+    }
+
+    @Override
+    public double getAverageSamplingPeriod() {
+
+        long accumulator = 0;
+
+        synchronized (histogramLock) {
+
+            for (int idx = 0; idx < MAX_NUM_TIMING_SAMPLES; ++idx) {
+
+                accumulator += timingHistogram[idx];
+            }
+
+        }
+        return accumulator / (double) MAX_NUM_TIMING_SAMPLES;
+
+    }
+
+    //TODO This section is commented out so the driver does not attempt to send an alert while the email target and sender is not set up. check the read me document for details.
+    // lines 299-306 and 322-328 are commented out for the same reason.
+//    public void sendCPUEmail() {
+//        try {
+//            properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+//            properties.setProperty("mail.smtp.port", "587");
+//            properties.setProperty("mail.smtp.auth", "true");
+//            properties.setProperty("mail.smtp.starttls.enable", "true");
+//
+//            MimeMessage message = new MimeMessage(session);
+//            message.setFrom(alertTarget);
+//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(alertTarget));
+//
+//            message.setSubject("The CPU Usage value for your box is above the threshold limit.");
+//            message.setText("The node associated with this Alert Target Email Address has reached a value about the set threshold.");
+//            Transport.send(message);
+//        } catch (MessagingException mex) {
+//            mex.printStackTrace();
+//        }
+//
+//    }
+//
+//    public void sendRAMEmail() {
+//        try {
+//            properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+//            properties.setProperty("mail.smtp.port", "587");
+//            properties.setProperty("mail.smtp.auth", "true");
+//            properties.setProperty("mail.smtp.starttls.enable", "true");
+//
+//            MimeMessage message = new MimeMessage(session);
+//            message.setFrom(alertTarget);
+//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(alertTarget));
+//
+//            message.setSubject("The RAM Usage value for your box is above the threshold limit.");
+//            message.setText("The node associated with this Alert Target Email Address has reached a value about the set threshold.");
+//            Transport.send(message);
+//        } catch (MessagingException mex) {
+//            mex.printStackTrace();
+//        }
+//
+//    }
+//
+//    public void sendUserEmail() {
+//        try {
+//            properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+//            properties.setProperty("mail.smtp.port", "587");
+//            properties.setProperty("mail.smtp.auth", "true");
+//            properties.setProperty("mail.smtp.starttls.enable", "true");
+//
+//            MimeMessage message = new MimeMessage(session);
+//            message.setFrom(alertTarget);
+//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(alertTarget));
+//
+//            message.setSubject("The User login time for your box is above the threshold limit.");
+//            message.setText("The node associated with this Alert Target Email Address has reached a value about the set threshold.");
+//            Transport.send(message);
+//        } catch (MessagingException mex) {
+//            mex.printStackTrace();
+//        }
+//
+//    }
+
+
+    public double setCpuVal() {
+        double cpuLoad = processor.getSystemCpuLoad(1500) * 100;
+
+        return cpuLoad;
+
+    }
+
+
+    @Override
+    public void run() {
+
+
+        boolean processSets = true;
+
+
+        long lastSetTimeMillis = System.currentTimeMillis();
+
+
+        try {
+
+            while (processSets) {
+
+                DataBlock dataBlock;
+                if (latestRecord == null) {
+
+                    dataBlock = dataStruct.createDataBlock();
+
+                } else {
+
+                    dataBlock = latestRecord.renew();
+                }
+
+                synchronized (histogramLock) {
+
+                    int setIndex = setCount % MAX_NUM_TIMING_SAMPLES;
+
+                    // Get a sampling time for latest set based on previous set sampling time
+                    timingHistogram[setIndex] = System.currentTimeMillis() - lastSetTimeMillis;
+
+                    // Set latest sampling time to now
+                    lastSetTimeMillis = timingHistogram[setIndex];
+                }
+
+                ++setCount;
+                String cpuFormat = "System CPU Load: %.2f%%\n" + setCpuVal();
+
+                double timestamp = System.currentTimeMillis() / 1000d;
+
+
+                parentSensor.getLogger().trace(String.valueOf(timestamp), cpuFormat);
+                String Memory2 = String.valueOf(hal.getMemory());
+                String[] memoryUBStr = Memory2.split(" ");
+
+                float memoryVal1 = Float.parseFloat(memoryUBStr[1]);
+                String[] memoryBStr2 = memoryUBStr[2].split("/");
+                float memoryVal2 = Float.parseFloat(memoryBStr2[1]);
+
+
+                float memoryValUsage = memoryVal2 - memoryVal1;
+                float RAMAlertVal = (memoryValUsage / memoryVal2) * 100;
+                //UB and B stand for UnBroken and Broken respectively.
+
+                String cpuAlertBStr = cpuFormat.split("\n")[1];
+                double cpuAlertVal = Double.parseDouble(cpuAlertBStr);
+
+
+
+
+                dataBlock.setDoubleValue(0, timestamp);
+                dataBlock.setDoubleValue(1,  cpuAlertVal);
+                dataBlock.setStringValue(2, Memory2);
+
+
+
+
+//                if (!CPUemailSent) {
+//
+//                    if (cpuAlertVal >= 75.00) {
+////                        sendCPUEmail();
+//                        CPUemailSent = true;
+//
+//                    }
+//                }
+
+//              This is currently commented out b/c it's threshold value is not yet implemented
+
+//                if (userEmailSent =! true){
+//                    if (userAlertVal >= 75.00) {
+//                        sendCPUEmail();
+//                        userEmailSent = true;
+//
+//                    }
+//                }
+
+
+
+
+
+//                if (!RAMemailSent){
+//                    if (RAMAlertVal >= 75.00) {
+//                        sendRAMEmail();
+//                        RAMemailSent = true;
+//
+//                    }
+//                }
+
+                latestRecord = dataBlock;
+
+                latestRecordTime = System.currentTimeMillis();
+
+                eventHandler.publish(new DataEvent(latestRecordTime, Alerts.this, dataBlock));
+                synchronized (processingLock) {
+
+                    processSets = !stopProcessing;
+                }
+            }
+
+        } catch (Exception e) {
+
+            logger.error("Error in worker thread: {}", Thread.currentThread().getName(), e);
+
+        } finally {
+
+            // Reset the flag so that when driver is restarted loop thread continues
+            // until doStop called on the output again
+            stopProcessing = false;
+
+            logger.debug("Terminating worker thread: {}", this.name);
+        }
+    }
+}
---
+++ sensors/cyber/sensorhub-driver-sysinfo/sensorhub-driver-sysinfo/src/main/java/com/sample/impl/sensor/sysinfo/StorageOutput.java
@@ -0,0 +1,282 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ The contents of this file are subject to the Mozilla Public License, v. 2.0.
+ If a copy of the MPL was not distributed with this file, You can obtain one
+ at http://mozilla.org/MPL/2.0/.
+
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the License.
+
+ Copyright (C) 2024 Botts Innovative Research, Inc. All Rights Reserved.
+
+ ******************************* END LICENSE BLOCK ***************************/
+package com.sample.impl.sensor.sysinfo;
+
+import net.opengis.swe.v20.*;
+import org.sensorhub.api.data.DataEvent;
+import org.sensorhub.impl.sensor.AbstractSensorOutput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.vast.data.DataArrayImpl;
+import org.vast.swe.SWEHelper;
+import oshi.hardware.HWDiskStore;
+
+import java.util.List;
+import java.util.TimerTask;
+import java.util.Timer;
+
+
+
+public class StorageOutput extends AbstractSensorOutput<SystemsInfoSensor> {
+    private static final String DISK_SENSOR_OUTPUT_NAME = "Storage Systems info";
+    private static final String SENSOR_OUTPUT_LABEL = "Storage Systems info";
+    private static final String SENSOR_OUTPUT_DESCRIPTION = "Disk Storage Metrics returned from computer system info";
+    private static final Logger logger = LoggerFactory.getLogger(SystemsInfoOutput.class);
+    private static final int MAX_NUM_TIMING_SAMPLES = 10;
+    private final long[] timingHistogram = new long[MAX_NUM_TIMING_SAMPLES];
+    private final Object histogramLock = new Object();
+    Timer timerStorageOutput = new Timer();
+    TimerTask timerTask;
+    boolean isRunning;
+
+
+    oshi.SystemInfo si = new oshi.SystemInfo();
+
+
+    private final Object processingLock = new Object();
+
+    private DataRecord dataStruct;
+    private DataEncoding dataEncoding;
+
+
+    StorageOutput(SystemsInfoSensor parentSystemsInfoSensor) {
+
+        super(DISK_SENSOR_OUTPUT_NAME, parentSystemsInfoSensor, LoggerFactory.getLogger(SystemsInfoOutput.class));
+
+        logger.debug("Output created");
+    }
+
+
+    public void doStart() {
+
+        // Instantiate a new worker thread
+//        Runnable task =  this::run;
+        if (!isRunning) {
+
+           completeTask();
+           System.out.println("Timertask started");
+
+        }
+
+
+        System.out.println("Timertask started");
+
+    }
+
+    public boolean isAlive() {
+
+        return true;
+    }
+
+    @Override
+    public DataComponent getRecordDescription() {
+
+        return dataStruct;
+    }
+
+    @Override
+    public DataEncoding getRecommendedEncoding() {
+
+        return dataEncoding;
+    }
+
+    @Override
+    public double getAverageSamplingPeriod() {
+
+        long accumulator = 0;
+
+        synchronized (histogramLock) {
+
+            for (int idx = 0; idx < MAX_NUM_TIMING_SAMPLES; ++idx) {
+
+                accumulator += timingHistogram[idx];
+            }
+        }
+
+        return accumulator / (double) MAX_NUM_TIMING_SAMPLES;
+    }
+
+
+    public void doStop() {
+
+        if (isRunning) {
+            timerTask.cancel();
+            timerStorageOutput.cancel();
+            timerStorageOutput.purge();
+            isRunning = false;
+//            timerTask.cancel();
+
+
+            System.out.println("Timer task stopped");
+        }
+
+
+    }
+
+    private void initSamplingTime() {
+    }
+
+
+    protected void defineRecordStructure() {
+        SWEHelper sweFactory = new SWEHelper();
+
+
+        dataStruct = sweFactory.createRecord()
+                .name(DISK_SENSOR_OUTPUT_NAME)
+                .updatable(true)
+
+                .label(SENSOR_OUTPUT_LABEL)
+                .description(SENSOR_OUTPUT_DESCRIPTION)
+                .addField("SampleTime", sweFactory.createTime().asSamplingTimeIsoUTC().build())
+                .addField("diskStorageCount", sweFactory.createCount()
+                        .label("Disk Storage Count")
+                        .description("Number of returned disk drives on device")
+                        .definition(SWEHelper.getPropertyUri("disk_storage_count"))
+                        .id("diskStorageCount")
+                )
+                .addField("diskStorageArray", sweFactory.createArray()
+                        .label("Disk Storage Entries")
+                        .description("OSHI call for DiskStores- all storage devices on box that executes build")
+                        .withVariableSize("diskStorageCount")
+                        .withElement("diskStorage", sweFactory.createRecord()
+                                .label("Disk Storage")
+                                .description("Disk Storage Device found on box")
+                                .definition(SWEHelper.getPropertyUri("disk_storage"))
+
+                                .addField("storageDeviceNumber", sweFactory.createText()
+                                        .label("Device Number")
+                                        .description("Number assigned to disk device.")
+                                        .definition(SWEHelper.getPropertyUri("storage_dev_number"))
+                                )
+                                .addField("modelName", sweFactory.createText()
+                                        .label("Device Model")
+                                        .description("Model of Disk Device.")
+                                        .definition(SWEHelper.getPropertyUri("device_model"))
+                                )
+                                .addField("diskSize", sweFactory.createQuantity()
+                                        .label("Device Size")
+                                        .uomCode("B")
+                                        .description("Size of Disk Device.")
+                                        .definition(SWEHelper.getPropertyUri("device_size"))
+
+                                )
+                                .addField("diskRead", sweFactory.createQuantity()
+                                        .label("Device Read Size")
+                                        .description("Size of Disk Read GIB")
+                                        .uomCode("GB")
+                                        .definition(SWEHelper.getPropertyUri("device_read_size"))
+                                )
+                                .addField("diskWrite", sweFactory.createQuantity()
+                                        .label("Device Write Size")
+                                        .uomCode("GB")
+                                        .description("Size of Disk Write GIB")
+                                        .definition(SWEHelper.getPropertyUri("device_write_size"))
+                                )
+                        ))
+
+
+                .build();
+        dataEncoding = sweFactory.newTextEncoding(",", "\n");
+    }
+
+    public void doInit() {
+
+        getLogger().debug("Initializing Output");
+
+        defineRecordStructure();
+
+
+        initSamplingTime();
+
+        getLogger().debug("Initializing Output Complete");
+
+    }
+
+    private void completeTask() {
+        timerStorageOutput = new Timer();
+        timerTask = new TimerTask() {
+
+            @Override
+            public void run() {
+                synchronized (processingLock) {
+                    try {
+                        defineRecordStructure();
+                        executeDataStruct();
+                    } catch (Exception e) {
+                        logger.error("Error executing", e);
+                    }
+                }
+            }
+        };
+        timerStorageOutput.scheduleAtFixedRate(timerTask, 0, 5000);
+        isRunning = true;
+    }
+
+    public void executeDataStruct() {
+        DataBlock dataBlock;
+        dataBlock = dataStruct.createDataBlock();
+        double timestamp = System.currentTimeMillis() / 1000d;
+
+        defineRecordStructure();
+
+        int index = 0;
+
+
+        List<HWDiskStore> diskList = si.getHardware().getDiskStores();
+
+
+        defineRecordStructure();
+
+
+        int storageCount = si.getHardware().getDiskStores().size();
+
+        dataStruct.setData(dataBlock);
+
+        dataBlock.setDoubleValue(index++, timestamp);
+        dataBlock.setIntValue(index++, storageCount);
+
+        var diskStorageArray = ((DataArrayImpl) dataStruct.getComponent("diskStorageArray"));
+        diskStorageArray.updateSize();
+        dataBlock.updateAtomCount();
+
+
+        for (HWDiskStore disk : diskList) {
+
+
+            String modelName = disk.getModel();
+            String diskName = disk.getName();
+            long diskSize = disk.getSize();
+            long readSize = disk.getReads();
+            long writeSize = disk.getWrites();
+
+            dataBlock.setStringValue(index++, modelName);
+            dataBlock.setStringValue(index++, diskName);
+            dataBlock.setLongValue(index++, diskSize);
+            dataBlock.setLongValue(index++, readSize);
+            dataBlock.setLongValue(index++, writeSize);
+        }
+
+        latestRecord = dataBlock;
+
+        latestRecordTime = System.currentTimeMillis();
+
+        eventHandler.publish(new DataEvent(latestRecordTime, StorageOutput.this, dataBlock));
+    }
+
+    Timer getStorageOutputTimer() {
+        return timerStorageOutput;
+    }
+}
+
+
