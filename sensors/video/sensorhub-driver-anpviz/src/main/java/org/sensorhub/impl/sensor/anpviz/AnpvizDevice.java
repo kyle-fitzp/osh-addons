5717c58eae67513b927bf13dccc66a21419224f5
kyle-fitzp <kyle.fitzpatrick@botts-inc.com>
2025-07-31 13:26:00 -0500

    Added login functionality

--- sensors/video/sensorhub-driver-anpviz/src/main/java/org/sensorhub/impl/sensor/anpviz/AnpvizDevice.java
+++ sensors/video/sensorhub-driver-anpviz/src/main/java/org/sensorhub/impl/sensor/anpviz/AnpvizDevice.java
@@ -1,93 +1,137 @@
 package org.sensorhub.impl.sensor.anpviz;
 
-import java.io.ByteArrayOutputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.MalformedURLException;
-
-import jakarta.xml.soap.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import javax.crypto.Cipher;
-import javax.xml.XMLConstants;
-import javax.xml.namespace.QName;
+import javax.xml.*;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 public class AnpvizDevice {
+    private enum AnpvizRequest {
+        LOGIN_PATH("ipcLogin"),
+        USER_CONFIG_PATH("getUserConfigPwdEntrypt"),
+        MEDIA_CONFIG_PATH("getMediaVideoConfig"),
+        SYSTEM_VERSION_PATH("getSystemVersionInfo"),
+        PTZ_CONFIG_PATH("getPtzConfig"),
+        PRESET_LIST_PATH("getPresetList"),
+        TIME_CONFIG_PATH("getTimeConfig"),
+        MEDIA_STREAM_CONFIG_PATH("getMediaStreamConfig");
+
+        private AnpvizRequest(String requestPath) {
+            this.requestPath = requestPath;
+        };
+        public final String requestPath;
+    }
 
     private static final Logger logger = LoggerFactory.getLogger(AnpvizDevice.class);
-    private static final String KEY = "WebLogin";
-    private String SOAP_URI;
-    private String SOAP_PREFIX;
-    private static final String LOGIN_PATH = "ipcLogin";
-    private static final String USER_CONFIG_PATH = "getUserConfigPwdEntrypt"; // Yes, the "typo" is correct
-    private static final String MEDIA_CONFIG_PATH = "getMediaVideoConfig";
-    private static final String SYSTEM_VERSION_PATH = "getSystemVersionInfo";
-    private static final String PTZ_CONFIG_PATH = "getPtzConfig";
-    private static final String PRESET_LIST_PATH = "getPresetList";
-    private static final String TIME_CONFIG_PATH = "getTimeConfig";
-    private static final String MEDIA_STREAM_CONFIG_PATH = "getMediaStreamConfig";
+    private static final String KEY = "WebLogin"; // Encryption key was stored as plain text in the JS lol
 
     String remoteHost;
     int remotePort;
     String user;
     String rawPasswd;
+    String userGroup;
 
-    public AnpvizDevice(String remoteHost, int remotePort, String user, String passwd) throws SOAPException, MalformedURLException {
+    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+
+    public AnpvizDevice(String remoteHost, int remotePort, String user, String passwd) throws IOException, ParserConfigurationException, SAXException {
         this.remoteHost = remoteHost;
         this.user = user;
         this.rawPasswd = passwd;
         this.remotePort = remotePort;
-
-        SOAPConnectionFactory soapFactory = SOAPConnectionFactory.newInstance();
-        SOAPConnection soapConnection = soapFactory.createConnection();
-
-        SOAPMessage request = MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL).createMessage();
-        SOAPEnvelope envelope = request.getSOAPPart().getEnvelope();
-        SOAP_URI = envelope.getNamespaceURI();
-        SOAP_PREFIX = envelope.getPrefix();
-        envelope.addNamespaceDeclaration(SOAP_PREFIX, SOAP_URI);
-        SOAPHeader header = request.getSOAPHeader();
-        SOAPBody body = request.getSOAPBody();
-        header.setPrefix(SOAP_PREFIX);
-
-        addMimeHeaders(request.getMimeHeaders());
-        SOAPElement userId = header.addHeaderElement(new QName(SOAP_URI, "userid", SOAP_PREFIX));
-        userId.addTextNode(encryptDesHex(user, KEY));
-        SOAPElement passWd = header.addHeaderElement(new QName(SOAP_URI, "passwd", SOAP_PREFIX));
-        passWd.addTextNode(encryptDesHex(passwd, KEY));
-
-        SOAPMessage response = soapConnection.call(request, getEndpoint() + "/" + LOGIN_PATH);
-
-        try {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            request.writeTo(out);
-            String strMsg = new String(out.toByteArray());
-            logger.debug("Request: {}", strMsg);
-        } catch (Exception ignored) {}
-
-        try {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            response.writeTo(out);
-            String strMsg = new String(out.toByteArray());
-            logger.debug("Response: {}", strMsg);
-        } catch (Exception ignored) {}
+        login();
+        getUserConfig();
     }
 
-    private void addMimeHeaders(MimeHeaders headers) {
-        headers.setHeader("Accept", "text/javascript, text/html, application/xml, text/xml, */*");
-        headers.setHeader("Accept-Language", "en-US,en;q=0.9");
-        headers.setHeader("Connection", "keep-alive");
-        headers.setHeader("Content-type", "application/x-www-form-urlencoded");
-        headers.setHeader("Host", remoteHost);
-        headers.setHeader("Origin", getEndpointNoPort());
-        headers.setHeader("Referer", getEndpointNoPort() + "/");
-        headers.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36");
-        headers.setHeader("X-Requested-With", "XMLHttpRequest");
+    public void login() throws IOException {
+        sendRequest("", AnpvizRequest.LOGIN_PATH);
+    }
 
-        // Add cookies (use "Cookie" header)
-        String cookieValue = "DHLangCookie30=English; ipc_" + remoteHost + "_username=" + user + "; ipc_" + remoteHost + "_password=" + encryptMd5Hex(rawPasswd) + "; ipc_" + remoteHost + "_webLanguage=en_us; ipc_" + remoteHost + "_KeepScale=0";
-        headers.addHeader("Cookie", cookieValue);
+    public void getUserConfig() throws IOException, ParserConfigurationException, SAXException {
+        String response = sendRequest("",  AnpvizRequest.USER_CONFIG_PATH);
+        String groupAtt = "Group=\"";
+        String group = "";
+        int index = -1;
+        if ((index = response.indexOf(groupAtt, response.indexOf(user))) > -1) {
+            group = response.substring(index + groupAtt.length(), response.indexOf("\"", index + groupAtt.length() + 1));
+        }
+        this.userGroup = group;
+    }
+
+    // Have to do all the SOAP request generation/parsing manually; this camera does not
+    // use valid SOAP (or even well-formed XML).
+    private String sendRequest(String xmlBody, AnpvizRequest request) throws IOException {
+        String desUser = encryptDesHex(user, KEY).trim();
+        String desPass = encryptDesHex(rawPasswd, KEY).trim();
+
+        if (xmlBody == null) {
+            xmlBody = "";
+        }
+
+        String xml = """
+                <?xml version=\"1.0\"?>
+                <soap:Envelope xmlns:soap=\"http://www.w3.org/2001/12/soap-envelope\">
+                    <soap:Header>
+                        <userid>""" + desUser + """
+                        </userid>
+                        <passwd>""" + desPass + "</passwd>"
+                +"""
+                    </soap:Header>
+                    <soap:Body>""" + xmlBody + "</soap:Body>"
+                +"</soap:Envelope>";
+
+
+        URL url = new URL(getEndpoint() + "/" + request.requestPath);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+
+        conn.setRequestProperty("Accept", "text/javascript, text/html, application/xml, text/xml, */*");
+        conn.setRequestProperty("Accept-Language", "en-US,en;q=0.9");
+        conn.setRequestProperty("Connection", "keep-alive");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Host", remoteHost);
+        conn.setRequestProperty("Origin", getEndpointNoPort());
+        conn.setRequestProperty("Referer", getEndpointNoPort() + "/");
+        conn.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+        conn.setRequestProperty("Cookie", getCookieHeader());
+
+        logger.debug("Sending XML:\n{}", xml);
+
+        try (OutputStream os = conn.getOutputStream()) {
+            os.write(xml.getBytes(StandardCharsets.UTF_8));
+        }
+
+        int responseCode = conn.getResponseCode();
+        logger.debug("Response Code: {}", responseCode);
+
+        StringBuilder response = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                logger.debug(line);
+                response.append(line);
+            }
+        }
+        return response.toString();
+    }
+
+    private String getCookieHeader() {
+        return "DHLangCookie30=English; ipc_" + remoteHost + "_username=" + user +
+                "; ipc_" + remoteHost + "_password=" + encryptMd5Hex(rawPasswd) +
+                "; ipc_" + remoteHost + "_webLanguage=en_us; ipc_" + remoteHost + "_KeepScale=0";
     }
 
     public String getEndpoint() {
@@ -100,15 +144,18 @@ public class AnpvizDevice {
 
     private String encryptDesHex(String input, String key) {
         try {
-            byte[] keyBytes = key.substring(0, 8).getBytes("UTF-8");
+            byte[] keyBytes = key.substring(0, 8).getBytes(StandardCharsets.UTF_8);
             SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "DES");
-
             Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding");
 
-            byte[] inputBytes = input.getBytes("UTF-8");
+            byte[] inputBytes = input.getBytes(StandardCharsets.UTF_8);
+            int padLength = 8 - inputBytes.length % 8;
+            byte[] padded = new byte[inputBytes.length + padLength];
+            System.arraycopy(inputBytes, 0, padded, 0, inputBytes.length);
+            /*
             byte[] padded = new byte[8];
             System.arraycopy(inputBytes, 0, padded, 0, Math.min(inputBytes.length, 8));
-
+             */
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             byte[] encrypted = cipher.doFinal(padded);
 
@@ -124,18 +171,15 @@ public class AnpvizDevice {
         return null;
     }
 
-    // TODO
     private String encryptMd5Hex(String input) {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             byte[] digest = md.digest(input.getBytes("UTF-8"));
 
-            // Convert to uppercase hex
             StringBuilder hexString = new StringBuilder();
             for (byte b : digest) {
                 hexString.append(String.format("%02X", b));
             }
-
             return hexString.toString();
         } catch (Exception e) {
             logger.error("Could not generate MD5", e);
